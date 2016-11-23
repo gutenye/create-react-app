733c58f170ade4ca2d0781055ce612007c11c19d
Guten Ye <ywzhaifei@gmail.com>
2016-11-23 11:15:53 +0800

    b

--- __readme__.md
+++ __readme__.md
@@ -1,8 +1,17 @@
 Changes: https://github.com/facebookincubator/create-react-app/compare/master...gutenye:master
 
+
 1. Only change packages/react-scripts and eslint-config-react-app two packages.
 2. Change babel-preset config in packages/react-scripts/config/webpack.config.dev.js
-3. If react-dev-utils is outdated, wait for it release.
+
+Update
+
+```
+$ check release tag https://github.com/facebookincubator/create-react-app/releases
+$ git fetch --tags origin
+$ git merge v0.7.0
+> For react-dev-utils maybe not released, manually fix has ton of bugs.
+```
 
 Publish
 
--- packages/react-scripts/scripts/start.js
+++ packages/react-scripts/scripts/start.js
@@ -26,7 +26,7 @@ var detect = require('detect-port');
 var clearConsole = require('react-dev-utils/clearConsole');
 var checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
 var formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
-var getProcessForPort = require('react-dev-utils/getProcessForPort');
+//var getProcessForPort = require('react-dev-utils/getProcessForPort');
 var openBrowser = require('react-dev-utils/openBrowser');
 var prompt = require('react-dev-utils/prompt');
 var pathExists = require('path-exists');
@@ -281,7 +281,8 @@ detect(DEFAULT_PORT).then(port => {
   }
 
   clearConsole();
-  var existingProcess = getProcessForPort(DEFAULT_PORT);
+  //var existingProcess = getProcessForPort(DEFAULT_PORT);
+  var existingProcess = false
   var question =
     chalk.yellow('Something is already running on port ' + DEFAULT_PORT + '.' +
       ((existingProcess) ? ' Probably:\n  ' + existingProcess : '')) +
